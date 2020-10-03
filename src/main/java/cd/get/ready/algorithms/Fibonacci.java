db1bdf8afcbc292b5a2f988eb2e52e4d7905be1b
Isaiah Williams <isaiah@Isaiahs-MacBook-Pro-5.local>
2020-10-03 19:15:22 -0400

    "Isaiah Problem 6 Done Oct 3rd"

--- src/main/java/cd/get/ready/algorithms/Fibonacci.java
+++ src/main/java/cd/get/ready/algorithms/Fibonacci.java
@@ -1,7 +1,21 @@
 package cd.get.ready.algorithms;
 
 public class Fibonacci {
-    public int getFibonacciValue(int n){
-        return -1;
+    public int getFibonacciValue(int n) {
+
+        if(n==0){
+            return 0;
+        }
+        int thisOne = 1;
+        int last = 0;
+
+        int beforeLast;
+        for (int i = 2; i <= n; i++) {
+
+            beforeLast = last;
+            last = thisOne;
+            thisOne = beforeLast + last;
+        }
+        return thisOne;
     }
 }
